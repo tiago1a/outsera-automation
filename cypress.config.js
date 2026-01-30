773aa0094478006e0df20f7580a42686ef20c2f0
Tiago Amaro <tiagoamaro01@gmail.com>
2026-01-30 13:23:43 -0300

    iniciando configuracoes da papiline

--- cypress.config.js
+++ cypress.config.js
@@ -5,13 +5,18 @@ const addCucumberPreprocessorPlugin =
 const createEsbuildPlugin =
   require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
+// Import do mochawesome reporter
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
+
 module.exports = defineConfig({
   e2e: {
     testIsolation: false,
     specPattern: "cypress/e2e/features/**/*.feature",
     async setupNodeEvents(on, config) {
+      // mantém o cucumber
       await addCucumberPreprocessorPlugin(on, config);
 
+      // mantém o esbuild
       on(
         "file:preprocessor",
         createBundler({
@@ -19,8 +24,20 @@ module.exports = defineConfig({
         })
       );
 
+      // adiciona o mochawesome reporter
+      mochawesome(on);
+
       return config;
     },
     baseUrl: "https://www.saucedemo.com",
+  },
+
+  // configuração do reporter
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "reports",
+    overwrite: false,
+    html: true,
+    json: true,
   },
 });
