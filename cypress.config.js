bec279eba0babb555aef9dfd44f9f45bc4483566
Tiago Amaro <tiagoamaro01@gmail.com>
2026-01-30 16:44:25 -0300

    atualizacao de config

--- cypress.config.js
+++ cypress.config.js
@@ -10,32 +10,21 @@ module.exports = defineConfig({
   e2e: {
     testIsolation: false,
     specPattern: "cypress/e2e/features/**/*.feature",
+
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
 
       on(
         "file:preprocessor",
         createBundler({
-          platform: "node",   // 👈 força esbuild a usar built-ins do Node
-          format: "cjs",      // 👈 garante suporte a require()
-          target: ["node16"], // 👈 compatível com Node no container
-          plugins: [
-            createEsbuildPlugin(config),
-            {
-              name: "crypto-polyfill",
-              setup(build) {
-                build.onResolve({ filter: /^crypto$/ }, () => ({
-                  path: require.resolve("crypto-browserify"),
-                }));
-              },
-            },
-          ],
+          plugins: [createEsbuildPlugin(config)],
         })
       );
 
       mochawesome(on);
       return config;
     },
+
     baseUrl: "https://www.saucedemo.com",
   },
 
